4b01a695840b8ab26a20d0bdf05e8418462cecdd
kebele <ykemalbeler@gmail.com>
2020-09-27 02:24:20 +0300

    ekleme

---
+++ todo_app/src/views/Home4.vue
@@ -0,0 +1,56 @@
+ <template>
+     <div>
+ <v-container pa-0>
+          <v-row no-gutters>
+            <v-col cols="12" sm="6" offset-sm="3">
+              <v-card
+                class="pa-2"
+                outlined
+                tile
+              >
+                Column
+              </v-card>
+            </v-col>
+          </v-row>
+          <v-row justify="space-between">
+            <v-col cols="4">
+              <v-card
+                class="pa-2"
+                outlined
+                tile
+              >
+                Column
+              </v-card>
+            </v-col>
+            <v-col cols="4">
+              <v-card
+                class="pa-2"
+                outlined
+                tile
+              >
+                Column
+              </v-card>
+            </v-col>
+            <!-- <v-col cols="4">
+              <v-card
+                class="pa-2"
+                outlined
+                tile
+              >
+                Column
+              </v-card>
+            </v-col> -->
+          </v-row>
+        </v-container>
+     </div>
+ </template>
+ 
+ <script>
+     export default {
+         
+     }
+ </script>
+ 
+ <style lang="scss" scoped>
+ 
+ </style>
