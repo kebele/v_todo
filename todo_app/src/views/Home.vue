bc9afc960aba599d0e527ef6fd7eea675a9086c3
kebele <ykemalbeler@gmail.com>
2020-09-26 21:04:04 +0300

    ekleme

--- todo_app/src/views/Home.vue
+++ todo_app/src/views/Home.vue
@@ -1,41 +1,43 @@
 <template>
   <div class="home">
     <h1>homepage</h1>
-    <!-- buton ve icon kullanımı -->
+    <!-- spacing - margin-padding -->
+    <!-- m margin, p padding bootstrap gibi kullanımı var, sm small screen gibi,   -->
+        <h3>margin - padding - spacing</h3>
+    <v-row>
+      <h3 class="ml-sm-12">ml small screen</h3>
+      <h3 class="ml-10">margin left</h3>
+      <h3 class="mx-auto">Spacing</h3>
+    </v-row>
+    <br />
+    <!-- buttons  -->
+    <v-row class="ml-15">
+      <h3>buttons</h3>
 
-    <v-btn class="green white--text">click me</v-btn>
-    <v-btn depressed color="green">click me</v-btn>
-    <v-btn flat color="green">click me</v-btn>
-
-    <v-btn depressed class="pink white--text">
-        <v-icon left>mdi-email</v-icon>
-        <span>email me</span>
-    </v-btn>
-
-    <v-btn depressed small class="blue white--text">
-        <v-icon left small>mdi-email</v-icon>
-        <span>email me</span>
-    </v-btn>
-
-    <v-btn depressed large class="orange white--text">
-        <span>email me</span>
-        <v-icon right large>mdi-email</v-icon>
-    </v-btn>
-<!-- https://cdn.materialdesignicons.com/5.4.55/ -->
-    <v-btn fab depressed small dark color="purple">        
-        <v-icon large>mdi-heart</v-icon>
-        <!-- <v-icon large>mdi-Heart-Multiple</v-icon> -->
-        
-    </v-btn>
+      <v-row class="mt-5 mb-5">
+        <v-col cols="12">
+          
+            <v-btn small outlined block>submit</v-btn>
+          
+          
+            <v-btn class="red white--text" small rounded>submit</v-btn>
+    <v-col>
+        <v-btn fab color="warning">
+                <v-icon>mdi-pencil</v-icon>
+            </v-btn>
+    </v-col>
+            
+          
+        </v-col>
+      </v-row>
+    </v-row>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 
-
 export default {
-  name: 'Home',
- 
-}
+  name: "Home",
+};
 </script>
